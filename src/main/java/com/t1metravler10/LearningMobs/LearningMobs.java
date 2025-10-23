dba1202f99df337f88e11f12f930933a99b3b68c
T1metravler10 <T1metravler10@users.noreply.github.com>
2025-10-23 11:13:01 -0400

    Remove custom creative tab registration

--- src/main/java/com/t1metravler10/LearningMobs/LearningMobs.java
+++ src/main/java/com/t1metravler10/LearningMobs/LearningMobs.java
@@ -5,13 +5,9 @@ import com.t1metravler10.LearningMobs.ai.CreeperSquadManager;
 import com.t1metravler10.LearningMobs.ai.MobGenerationManager;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -42,10 +38,8 @@ public class LearningMobs {
     public static final String MODID = "learningmobs";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    private static final ResourceKey<Registry<CreativeModeTab>> CREATIVE_TAB_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath("minecraft", "creative_mode_tab"));
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(CREATIVE_TAB_KEY, MODID);
 
     public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
@@ -53,12 +47,6 @@ public class LearningMobs {
     public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(
             new FoodProperties.Builder().alwaysEdible().nutrition(1).saturationModifier(2f).build()
     )));
-
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> output.accept(EXAMPLE_ITEM.get()))
-            .build());
 
     public LearningMobs(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
@@ -68,8 +56,6 @@ public class LearningMobs {
 
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
-        CREATIVE_MODE_TABS.register(modEventBus);
-
         MinecraftForge.EVENT_BUS.register(this);
         context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
